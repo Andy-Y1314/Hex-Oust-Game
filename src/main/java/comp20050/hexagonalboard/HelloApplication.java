ec3c4a81fd0835e33ecc39491b82c62c391dc56f
Matthew <matthewj.oconnor01@gmail.com>
2025-03-10 11:19:23 +0000

    Adjusted default window size

--- src/main/java/comp20050/hexagonalboard/HelloApplication.java
+++ src/main/java/comp20050/hexagonalboard/HelloApplication.java
@@ -17,6 +17,7 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("The Ultimate Businessmen Hex Oust");
         stage.setScene(scene);
+        stage.setMaximized(true); // app will always launch at maximum window size
         stage.show();
     }
 
