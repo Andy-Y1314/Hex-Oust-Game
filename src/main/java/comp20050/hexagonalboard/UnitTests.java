5f3765455aa26d3c4b0451f76f7f4f8b1d8fd6e8
HasanHasanUCD <hasan.hasan@ucdconnect.ie>
2025-04-29 19:31:51 +0100

    cleaned up a bit of code

--- src/main/java/comp20050/hexagonalboard/UnitTests.java
+++ src/main/java/comp20050/hexagonalboard/UnitTests.java
@@ -11,10 +11,21 @@ import static org.junit.Assert.*;
 
 public class UnitTests {
 
+    /**
+     * Hexagon Class tests
+     */
     @Test
-    public void testCoordinatesToId() {
+    public void testCoordinatesToId1() {
         int q = 4, r = -1, s = -3;
         String expected = "q4rm1sm3";
+
+        assertEquals(expected, Hexagon.coordinatesToId(q, r, s));
+    }
+
+    @Test
+    public void testCoordinatesToId2() {
+        int q = -2, r = 2, s = 0;
+        String expected = "qm2r2s0";
 
         assertEquals(expected, Hexagon.coordinatesToId(q, r, s));
     }
@@ -33,55 +44,43 @@ public class UnitTests {
     }
 
     @Test
-    public void placingStoneNonCapturing() {
-        String inputId = "q0r0s0";
-        Color currPlayerColor = Color.RED;
+    public void testHexagonIsSameColor() {
+        //Hexagon q,r,s values are trivial
+        Hexagon redHex = new Hexagon(2,3,4);
+        Hexagon redHex2 = new Hexagon(3,4,5);
+        Hexagon blueHex = new Hexagon(1,0,1);
 
-        Board input = new Board(7);
+        redHex.setColor(Color.RED);
+        redHex2.setColor(Color.RED);
+        blueHex.setColor(Color.BLUE);
 
-        Board expected = new Board(7);
-        expected.getHexagonById("q0r0s0").setColor(Color.RED);
-
-
-        Hexagon hex = input.getHexagonById(inputId);
-
-        List<Hexagon> hexToDelete = input.validateMove(hex, currPlayerColor);
-
-        /*
-        - Copied directly from Controller class logic (getHexId)
-        - Frontend / UI functionality has been commented out
-         */
-
-        if (hexToDelete == null) {
-            //displayInvalidMove();
-        } else {
-            hex.setColor(Color.RED);
-            //updateHexCounter(currentPlayer.getColor(), true);
-            //polygonSetColor(hex.getId(), hex.getColor());
-
-            for (Hexagon toDelete : hexToDelete) {
-                toDelete.setColor(colorGrey);
-                //updateHexCounter(currentPlayer.getEnemyColor(), false);
-                //polygonSetColor(toDelete.getId(), colorGrey);
-            }
-        }
-
-        /*
-        for (int i = 0; i < this.board.size(); i++) {
-            for (int j = 0; j < this.board.get(i).size(); j++) {
-                if (!this.board.get(i).get(j).sameColor(board.board.get(i).get(j))) {
-                    return false;
-                }
-            }
-        }
-         */
-
+        assertFalse(redHex.isSameColor(blueHex));
+        assertTrue(redHex.isSameColor(redHex2));
     }
+
+    @Test
+    public void testHexagonIsEnemyColor() {
+        //Hexagon q,r,s values are trivial
+        Hexagon redHex = new Hexagon(0,0,0);
+        Hexagon blueHex = new Hexagon(4,2,0);
+        Hexagon blueHex2 = new Hexagon(0,6,9);
+
+        redHex.setColor(Color.RED);
+        blueHex.setColor(Color.BLUE);
+        blueHex2.setColor(Color.BLUE);
+
+        assertTrue(blueHex.isEnemyColor(redHex));
+        assertFalse(blueHex.isEnemyColor(blueHex2));
+    }
+
+
+    /**
+     * Board Class Tests
+     */
 
     @Test
     public void testGetNeighbours() {
         Board input = new Board(7);
-
         Hexagon hex = input.getHexagonById("q0r0s0");
 
         //Creating a list of all the expected neighbours the 'getNeighbours' method should return for hexagon q0r0s0
@@ -209,5 +208,54 @@ public class UnitTests {
         List<String> wrongEnemyHexagons = List.of("q0rm1s1", "qm1rm1s2", "qm1r0s1");
 
         assertNotEquals(wrongEnemyHexagons, enemyHexagonIds);
+    }
+
+    /**
+     * Controller Class Tests
+     */
+    @Test
+    public void placingStoneNonCapturing() {
+        String inputId = "q0r0s0";
+        Color currPlayerColor = Color.RED;
+
+        Board input = new Board(7);
+
+        Board expected = new Board(7);
+        expected.getHexagonById("q0r0s0").setColor(Color.RED);
+
+
+        Hexagon hex = input.getHexagonById(inputId);
+
+        List<Hexagon> hexToDelete = input.validateMove(hex, currPlayerColor);
+
+        /*
+        - Copied directly from Controller class logic (getHexId)
+        - Frontend / UI functionality has been commented out
+         */
+
+        if (hexToDelete == null) {
+            //displayInvalidMove();
+        } else {
+            hex.setColor(Color.RED);
+            //updateHexCounter(currentPlayer.getColor(), true);
+            //polygonSetColor(hex.getId(), hex.getColor());
+
+            for (Hexagon toDelete : hexToDelete) {
+                toDelete.setColor(colorGrey);
+                //updateHexCounter(currentPlayer.getEnemyColor(), false);
+                //polygonSetColor(toDelete.getId(), colorGrey);
+            }
+        }
+
+        /*
+        for (int i = 0; i < this.board.size(); i++) {
+            for (int j = 0; j < this.board.get(i).size(); j++) {
+                if (!this.board.get(i).get(j).sameColor(board.board.get(i).get(j))) {
+                    return false;
+                }
+            }
+        }
+         */
+
     }
 }
